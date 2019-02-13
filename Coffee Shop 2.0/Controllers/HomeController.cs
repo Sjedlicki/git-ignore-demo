a92432bf6eb658711b42eaeff7b9570e987feb7f
stevojed <jedi1014@comcast.net>
2019-02-13 09:29:19 -0500

    added project

---
+++ Coffee Shop 2.0/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Coffee_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coffee_Shop_2._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "The Coffee Shop only provides the best coffees from around the world!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Summary(User u)
+        {
+            ViewBag.First = u.Firstname;
+            ViewBag.Last = u.Lastname;
+            ViewBag.Email = u.Email;
+            ViewBag.Pass = u.Password;
+            ViewBag.Phone = u.Phone;
+            return View();
+        }
+    }
+}
